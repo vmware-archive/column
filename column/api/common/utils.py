187e4371247b9ae0334d69714bedb74e67537c5d
Sihan Wang <wsihan@vmware.com>
2017-07-19 21:31:38 +0000

    Add api payload validator
    This patch adds run post payload validation
    
    Signed-off-by: Sihan Wang <wsihan@vmware.com>

--- column/api/common/utils.py
+++ column/api/common/utils.py
@@ -2,9 +2,22 @@
 # SPDX-License-Identifier: BSD-2-Clause
 
 import cerberus
+from flask import request
 
 
 def validate_payload(schema, payload):
     v = cerberus.Validator(schema)
     res = v.validate(payload)
     return res, v.errors
+
+
+def validator(schema, status_code):
+    def decorator(f):
+        def wrapper(controller_object):
+            payload = request.get_json(silent=True)
+            res, errors = validate_payload(schema, payload)
+            if not res:
+                return errors, status_code
+            return f(controller_object)
+        return wrapper
+    return decorator
