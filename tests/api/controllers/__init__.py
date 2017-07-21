82bf89a3284a3ebee7449cfbd39723700a52420e
Eric Brown <browne@vmware.com>
2017-07-21 16:14:47 -0700

    Add uwsgi support
    Signed-off-by: Eric Brown <browne@vmware.com>

--- tests/api/controllers/__init__.py
+++ tests/api/controllers/__init__.py
@@ -3,15 +3,15 @@
 
 import testtools
 
-from column.api.run import app
+from column.api.run import application
 
 
 class APITest(testtools.TestCase):
 
     def setUp(self):
         super(APITest, self).setUp()
-        app.config['SERVER_NAME'] = '127.0.0.1:48620'
+        application.config['SERVER_NAME'] = '127.0.0.1:48620'
         self.pb_running_time = 60
         self.wait_interval = 5
         self.counter = self.pb_running_time / self.wait_interval
-        self.app = app.test_client()
+        self.app = application.test_client()
