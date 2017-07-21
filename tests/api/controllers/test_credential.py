116483f212fda4712b389c000666fc5aa2615efc
Sihan Wang <wsihan@vmware.com>
2017-07-21 19:05:43 +0000

    Add payload validator for credential api
    Signed-off-by: Sihan Wang <wsihan@vmware.com>

--- tests/api/controllers/test_credential.py
+++ tests/api/controllers/test_credential.py
@@ -11,6 +11,14 @@ from tests.api import controllers
 
 class TestCredential(controllers.APITest):
 
+    def test_bad_payload(self):
+        response = self.app.get('/credentials')
+        self.assertEqual(http_client.BAD_REQUEST, response.status_code)
+        response = self.app.put('/credentials', data=json.dumps(
+            dict(bad_payload='test',)),
+            content_type='application/json')
+        self.assertEqual(http_client.BAD_REQUEST, response.status_code)
+
     @patch('column.utils.vault_decrypt')
     def test_get_credential(self, mock_vault_decrypt):
         mock_vault_decrypt.return_value = 'TestPassword'
