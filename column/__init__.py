9c225c1504a6c27e6fbb3f213b5f79f5b4e9d3e4
Eric Brown <browne@vmware.com>
2017-07-25 03:35:32 -0700

    Bump version: 0.3.12 → 0.3.13
    Signed-off-by: Eric Brown <browne@vmware.com>

--- column/__init__.py
+++ column/__init__.py
@@ -17,7 +17,7 @@ __all__ = [
 ]
 
 
-__version__ = '0.3.12'
+__version__ = '0.3.13'
 
 defaults = {
     'log_file': os.path.join(os.sep, 'var', 'log', 'column.log'),
--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ from setuptools import setup
 
 setup(
     name='column',
-    version='0.3.12',
+    version='0.3.13',
     url='https://github.com/vmware/column',
     license='BSD-2',
     author='VMware',
