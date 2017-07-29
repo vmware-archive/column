7608db2132a6289c3548fa7f135d5b7de1d2e006
Sihan Wang <wsihan@vmware.com>
2017-07-29 23:53:17 +0000

    Fix object sharing same address issue
    Signed-off-by: Sihan Wang <wsihan@vmware.com>

--- column/api/manager/__init__.py
+++ column/api/manager/__init__.py
@@ -4,13 +4,12 @@
 from column.api.manager import credential_manager
 from column.api.manager import run_manager
 
-_managers = {
-    'run': run_manager.RunManager(),
-    'credential': credential_manager.CredentialManager()
-}
-
 
 def get_manager(manager_type):
-    if manager_type in _managers:
-        return _managers[manager_type]
+    managers = {
+        'run': run_manager.RunManager(),
+        'credential': credential_manager.CredentialManager()
+    }
+    if manager_type in managers:
+        return managers[manager_type]
     raise NotImplementedError()
