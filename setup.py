5d4742ced00b4aa1908751bf22befe62aa021bda
Eric Brown <browne@vmware.com>
2017-07-31 00:59:54 -0700

    Bump version: 0.4.7 → 0.4.8
    Signed-off-by: Eric Brown <browne@vmware.com>

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ from setuptools import setup
 
 setup(
     name='column',
-    version='0.4.7',
+    version='0.4.8',
     url='https://github.com/vmware/column',
     license='BSD-2',
     author='VMware',
