f399e933531eabfc7fbd93fe8e628c5847d87d2d
Sihan Wang <wsihan@vmware.com>
2017-08-14 20:10:10 +0000

    Add private attribute for format response function
    Some attributes like become_pass, conn_pass and etc don't need to
    be passed into response.
    
    Signed-off-by: Sihan Wang <wsihan@vmware.com>

--- column/api/model/run_model.py
+++ column/api/model/run_model.py
@@ -14,16 +14,15 @@ def get_progress(run):
 def format_response(run):
     # TODO(ushergod) need to create backend model class and
     # put this fucntion into the model class
+    private_attr = ['become_pass', 'conn_pass', 'private_key_file']
     response = defaultdict(dict)
     for item, value in run.iteritems():
         if item == 'api_runner':
             response['progress'] = get_progress(run)
         elif item == 'options':
             for sub_i, sub_v in value.iteritems():
-                if sub_i == 'become_pass':
-                    response[item][sub_i] = "***"
-                elif sub_i == 'conn_pass':
-                    response[item][sub_i] = "***"
+                if sub_i in private_attr:
+                    continue
                 else:
                     response[item][sub_i] = sub_v
         else:
